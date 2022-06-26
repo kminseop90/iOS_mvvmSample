4546bceee9c29c11ac419d4140aa0e4b2c86cb2f
Minseop <alstjqaos@gmail.com>
2022-06-26 18:26:48 +0900

    Initial Commit

---
+++ Shared/MVVMSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSampleApp.swift
+//  Shared
+//
+//  Created by 김민섭 on 2022/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
